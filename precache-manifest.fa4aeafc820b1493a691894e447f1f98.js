265ebd7b6037ed98fb2e89e253b0174cdb4deae2
OleksandrHlybchenko <logista@meta.ua>
2019-07-04 00:01:47 +0300

    Updates

---
+++ precache-manifest.fa4aeafc820b1493a691894e447f1f98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd261435e135429c04a03d122c1bce35",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "30d4f44a3e11d8c84c74",
+    "url": "/react_static-list-of-posts/static/css/main.d28a5790.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "30d4f44a3e11d8c84c74",
+    "url": "/react_static-list-of-posts/static/js/main.918eeb8e.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
